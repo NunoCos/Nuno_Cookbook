bac9578794c4beb2fc1556005475e7611fa492c1
NunoCos <nuno18_9@hotmail.com>
2021-04-26 13:45:37 +0000

    style flash messages

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,11 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
     });
 
+/*
+Alert/Flash message
+*/
+
+$('#alert_close').click(function(){
+    $( "#alert_box" ).fadeOut( "slow", function() {
+    });
+  });
