efe43c07a4a627e4f9ea0356f61681534073bb26
NunoCos <nuno18_9@hotmail.com>
2021-04-26 10:30:51 +0000

    fix edit functionality

--- app.py
+++ app.py
@@ -111,9 +111,9 @@ def add_recipe():
     return render_template("add_recipe.html")
 
 
-@app.route("/edit_recipe/<recipe_id>", methods=["GET","POST"])
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
-    recipe = mongo.db.tasks.find_one({"_id":ObjectId(recipe_id)})
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("edit_recipe.html", recipe=recipe)
 
 
