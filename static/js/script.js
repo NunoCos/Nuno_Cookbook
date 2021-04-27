4549f50306ed0f4d665edea271ac67662a262ea5
NunoCos <nuno18_9@hotmail.com>
2021-04-27 08:10:06 +0000

    add slider to index

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,14 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
     });
 
-/*
-Alert/Flash message
-*/
+
+// Slider
+
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition:500,
+    interval: 6000
+});
 
